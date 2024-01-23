e3c14cd9150a0cd69fbb3adcd0f14dc1b5c16875
Rain Nyte <rain_nyte@proton.me>
2024-01-23 10:06:00 -0600

    fixed puts program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -9,6 +9,6 @@ int main(void)
 	/**
 	 *puts a string to the terminal
 	*/
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
