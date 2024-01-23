2823511fd86a937816346f8ff1aee834e068ddc9
Rain Nyte <rain_nyte@proton.me>
2024-01-23 12:06:51 -0600

    extra } deleted????

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
+	/**
+	 *seeing if adding a comment will get git to recognize that the file has
+	 *been edited
+	 */
 	if (n>0)
 	{
 		printf("%d is positive",n);
