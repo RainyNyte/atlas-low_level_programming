a63bc3525af7e6cff19653007563a2020ee1c524
Rain Nyte <rain_nyte@proton.me>
2024-01-23 10:15:33 -0600

    printf program

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- entrypoint
+ *
+ *Return: 0(always success
+ */
+int main(void)
+{
+	/**
+	 *prints the specified string exactly
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
