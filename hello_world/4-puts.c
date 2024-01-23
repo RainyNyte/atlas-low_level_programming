0835f18bffdaa941dc59a5917c1314d79e68d965
Rain Nyte <rain_nyte@proton.me>
2024-01-23 09:52:33 -0600

    broken puts program

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *puts - prints specified string to std output
+ *
+ *Return: 0 (always success)
+ */
+char puts("Programming is like building a multilingual puzzle)
+{
+	/**
+	 *puts a string to the terminal
+	*/
+	return (0);
+}
