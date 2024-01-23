397119e98777c192ab68eb9ab1b5c8c496e4f6bf
Rain Nyte <rain_nyte@proton.me>
2024-01-23 14:36:50 -0600

    now it's betty compliant

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,19 +18,19 @@ int main(void)
 	 *seeing if adding a comment will get git to recognize that the file has
 	 *been edited
 	 */
-	if (n>0)
+	if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		if (n==0)
+		if (n == 0)
 		{
-			printf("%d is zero\n",n);
+			printf("%d is zero\n", n);
 		}
 		else
 		{
-			printf("%d is negative\n",n);
+			printf("%d is negative\n", n);
 		}
 	}
 	return (0);
