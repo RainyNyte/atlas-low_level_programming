a33d2f2d3d29f841d33b8d2f551e17bbb666eb7f
Rain Nyte <rain_nyte@proton.me>
2024-02-05 12:38:43 -0600

    multiplication

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *mul- multiplies two numbers
+ *@a: one of the numbers to be multiplied
+ *@b: one of the numbers to be multiplied
+ *
+ *Return: the factor of the multiplication
+ */
+
 int mul(int a, int b)
 {
 	int factor;
