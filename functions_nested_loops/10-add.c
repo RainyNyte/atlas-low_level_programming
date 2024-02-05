639cc5a8e2f7f62d9f4f9713e3b229c5c02e672d
Rain Nyte <rain_nyte@proton.me>
2024-02-05 12:13:03 -0600

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add- adds two numbers together
+ *@a: one of the numbers to be added
+ *@b: one of the numbers to be added
+ *
+ *Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return(sum);
+}
