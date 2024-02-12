1c0771e0064b345118e7091a2da1500fc9526f45
Rain Nyte <rain_nyte@proton.me>
2024-02-12 13:09:18 -0600

    I have the power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_pow_recursion- calculates x to the power of y
+ *@x: the base number
+ *@y: the number that x is raised to the power of
+ *
+ *Return x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (x == 0)
+	{
+		return (0);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
