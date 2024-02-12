91aa4f6c8836e0c71100762c0db9c3ddc3963aa1
Rain Nyte <rain_nyte@proton.me>
2024-02-12 13:52:38 -0600

    squares

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *_sqrt_recursion- uses a recursive while loop to find the square root of
- *a number
+ *_sqrt_recursion- calls a helper function to calculate the sqrt of a number
  *@n: the number to find the square root of
  *
  *Return the square root of n
@@ -10,25 +9,29 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	return (helpy(n, 1));
+}
+
+/**
+ *helpy- a helper function for _sqrt_recursion that does the main math
+ *@x: the number that was passed to _sqrt_recursion
+ *@y: the number we are checking to see if it's sqr and x are equal
+ *
+ *Return: y if it is the sqrt of n, -1 if the number has no sqrt
+ */
+
+int helpy(int x, int y)
+{
+	if (y * y == x)
+	{
+		return (y);
+	}
+	if (y * y > x)
 	{
 		return (-1);
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
 	}
 	else
 	{
-		int i = 1;
-		while (i * i <= n)
-		{
-			if (i * i == n)
-			{
-				return (i);
-			}
-			i++;
-		}
-		return (-1);
+		return (helpy(x, y + 1));
 	}
 }
