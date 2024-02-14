ff7785f567a6e89b3fa7865390fb3f8107234ef3
Rain Nyte <rain_nyte@proton.me>
2024-02-13 19:19:25 -0600

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main- entrypoint
+ *@argc: number of arguments
+ *@argv: array containing arguments
+ *
+ *Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+    printf("%d\n", argc);
+}
