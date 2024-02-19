b530da7c2db0204184babdcca1899eea04b063f3
Rain Nyte <rain_nyte@proton.me>
2024-02-19 14:03:27 -0600

    now properly allocates memory for the string instead of throwing an error if the secons string is null n=but not the first

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,66 +13,53 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	 char *new_str;
-	 int len = 0;
-	 int g = 0;
-	int h = 0;
-        int i = 0;
-        int j = 0;
+    char *new_str;
+    int len = 0;
+    int g = 0;
+    int h = 0;
+    int i = 0;
+    int j = 0;
 
-	if (s1 != NULL)
-	{
-		while (s1[g] != '\0')
-		{
-			len++;
-			g++;
-		}
-	}
-	if (s2 != NULL)
-	{
-		while (s2[h] != '\0')
-		{
-			len++;
-			h++;
-		}
-	}
+    if (s1 != NULL)
+    {
+        while (s1[g] != '\0')
+        {
+            len++;
+            g++;
+        }
+    }
+    if (s2 != NULL)
+    {
+        while (s2[h] != '\0')
+        {
+            len++;
+            h++;
+        }
+    }
 
-	new_str = malloc(sizeof(char) * (len + 1));
+    new_str = malloc(sizeof(char) * (len + 1));
 
-	if (new_str == NULL)
-	{
-		return (NULL);
-	}
-	if (s1 == NULL)
-	{
-		while (s2[i] != '\0')
-		{
-			new_str[i] = s2[i];
-			i++;
-		}
-	}
-	if (s2 == NULL)
-	{
-		while (s1[i] != '\0')
-		{
-			new_str[i] = s1[i];
-			i++;
-		}
-	}
-	else
-	{
-		while (s1[i] != '\0')
-		{
-			new_str[i] = s1[i];
-			i++;
-		}
-		while (s2[j] != '\0')
-		{
-			new_str[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	new_str[i] = '\0';
-	return (new_str);
+    if (new_str == NULL)
+    {
+        return (NULL);
+    }
+    if (s1 != NULL)
+    {
+        while (s1[i] != '\0')
+        {
+            new_str[i] = s1[i];
+            i++;
+        }
+    }
+    if (s2 != NULL)
+    {
+        while (s2[j] != '\0')
+        {
+            new_str[i] = s2[j];
+            i++;
+            j++;
+        }
+    }
+    new_str[i] = '\0';
+    return (new_str);
 }
