61dd36f9c185caf06e62b9db4d8e393272b7f3d3
Rain Nyte <rain_nyte@proton.me>
2024-02-19 13:12:17 -0600

    mix and match

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -54,4 +54,5 @@ int is_prime_number(int n);
 int helpy_prime(int x, int y);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif
