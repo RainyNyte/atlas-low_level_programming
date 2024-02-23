5eab90983231123f9d94aa1a7343d7d16a7fe965
Rain Nyte <rain_nyte@proton.me>
2024-02-23 00:08:01 -0600

    who let the dogs out

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,6 +1,12 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ *free_dog- frees all allocated values of the struct passed to it, as well
+ *as the struct itself
+ *@d: the struct to be freed
+ */
+
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
