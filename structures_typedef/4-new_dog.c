2b1a78c36f2b29d8dc826489bdb28d7201eff9be
Rain Nyte <rain_nyte@proton.me>
2024-02-22 23:44:31 -0600

    a whole new dooooooooooog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,53 @@
 #include <stdlib.h>
 
 /**
+ *_strncpy- coppies at most the first n characters of a string
+ *@dest: string to coppy to
+ *@src: string to be coppied
+ *@n: maximum number of character to copy
+ *
+ *Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		for ( ; src[i] == '\0'; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
+	return (dest);
+}
+
+/**
+ *_strlen- finds the length of a string
+ *@s: pointer to the string that we are finding the length of
+ *
+ *Return: the length of the string located at *s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	if (s != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			len++;
+			i++;
+		}
+	}
+	return (len);
+}
+
+/**
  *new_dog- creates and initializes a new dog_t struct
  *@name: the name of the dog
  *@age the age of the dog
@@ -13,12 +60,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t  *dog;
+	char *n;
+	char *o;
+	int n_len;
+	int o_len;
+
+	n_len = _strlen(name);
+	o_len = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
+	n = malloc(sizeof(char) * (n_len + 1));
+	o = malloc(sizeof(char) * (o_len + 1));
 
-	dog->name = name;
+	n = _strcpy(n, name);
+	o = _strcpy(o, owner);
+	dog->name = n;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = o;
 
 	return(dog);
 }
