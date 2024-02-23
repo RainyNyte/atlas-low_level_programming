907d208c5c5957981f6e20a1fede18b3f61eb664
Rain Nyte <rain_nyte@proton.me>
2024-02-22 22:54:06 -0600

    dog data

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *print_dog- prints information about the dog struct that is passed to it
+ *prints nil if one of the members of the struct is NULL
+ *prints nothing if the struct passed into it is NULL
+ *@d: the struct that contains the information we will be printing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age == 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
