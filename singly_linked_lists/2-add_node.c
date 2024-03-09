ca5d8f428fddc75a413eb97285cd3d94d973f9c0
Rain Nyte <rain_nyte@proton.me>
2024-03-08 22:45:45 -0600

    betty coding style fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -37,6 +37,7 @@ int _strlen(const char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
@@ -47,6 +48,6 @@ list_t *add_node(list_t **head, const char *str)
 		new_node->len = _strlen(str);
 		new_node->next = *head;
 		*head = new_node;
-		return(new_node);
+		return (new_node);
 	}
 }
