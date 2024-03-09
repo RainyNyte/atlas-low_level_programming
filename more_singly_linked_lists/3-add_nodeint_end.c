f62e0a4c6992ded4d8abe87c69a0149a32efc6a6
Rain Nyte <rain_nyte@proton.me>
2024-03-08 23:59:43 -0600

    add int to end of list

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,40 +4,17 @@
 #include "lists.h"
 
 /**
- *_strlen- finds the length of a string
- *@s: pointer to the string that we are finding the length of
- *
- *Return: the length of the string located at *s
- */
-
-int _strlen(const char *s)
-{
-	int len = 0;
-	int i = 0;
-
-	if (s != NULL)
-	{
-		while (s[i] != '\0')
-		{
-			len++;
-			i++;
-		}
-	}
-	return (len);
-}
-
-/**
- *add_node_end- adds a node to the end  of a list_t list
+ *add_nodeint_end- adds a node to the end  of a listint_t list
  *@head: the starting node of the list
- *@str: the data to assign the str variable in the list_t struct
+ *@n: int to be assigned to the n value of the new neode
  *
  *Return: adress of the new node or NULL if failed
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *traveller = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *traveller = *head;
 
 	if (new_node == NULL)
 	{
@@ -45,8 +22,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		new_node->str = strdup(str);
-		new_node->len = _strlen(str);
+		new_node->n = n;
 		new_node->next = NULL;
 		if (*head == NULL)
 		{
