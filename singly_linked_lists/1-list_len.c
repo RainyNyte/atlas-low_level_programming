594ec98d5314db6887d7d396ef6e08e59590d18d
Rain Nyte <rain_nyte@proton.me>
2024-03-08 22:15:59 -0600

    count elements in list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include  "lists.h"
+
+/**
+ *list_len- counts the number of elements in a list_t list
+ *@h: the start of the list to count
+ *
+ *Return: the number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+	const list_t *traveller = h;
+
+	while (traveller != NULL)
+	{
+		n++;
+		traveller = traveller->next;
+	}
+	return (n);
+}
