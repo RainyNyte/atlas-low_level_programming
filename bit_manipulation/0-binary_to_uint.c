4f1ac51d45482ebfd4f61a58f7e2740058e96c97
Rain Nyte <rain_nyte@proton.me>
2024-03-09 21:01:42 -0600

    binary to uint

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *binary_to_uint- converts binary strings to uints
+ *@b: string of binary to convert to a uint
+ *
+ *Return:converted number, or 0 if b contains 1 or more characters that are not
+ *1 or 0 or b is NULL
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
