dff5686bce9d618bc955e7ceb734815d4bd4cd8a
Rain Nyte <rain_nyte@proton.me>
2024-03-11 18:46:13 -0500

    read and print

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -20,21 +20,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	 {
 		 return (0);
 	 }
+ 
 	 if (fd == -1)
 	 {
 		 return (0);
 	 }
+ 
 	 if (buf == NULL)
 	 {
 		 close(fd);
 		 return (0);
 	 }
+
 	 if (bytes_read == -1)
 	 {
 		 close(fd);
 		 free(buf);
 		 return (0);
 	 }
+
 	 if (bytes_written == -1 || bytes_written != bytes_read)
 	 {
 		 close(fd);
