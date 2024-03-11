11fb75aa8fe060f4b60679677dc25ecbaa070a1b
Rain Nyte <rain_nyte@proton.me>
2024-03-11 17:54:14 -0500

    flip bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits to flip to get from one number to another.
+ *flip_bits - Returns the number of bits to flip to get from
+ *one number to another.
+ *@n: The first unsigned long integer.
+ *@m: The second unsigned long integer.
  *
- * @n: The first unsigned long integer.
- * @m: The second unsigned long integer.
- *
- * Return: The number of bits to flip.
+ *Return: The number of bits to flip.
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
