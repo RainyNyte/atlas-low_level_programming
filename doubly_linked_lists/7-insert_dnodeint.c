84049f13aad38ad415da950428f1e9c24df6c215
Rain Nyte <rain_nyte@proton.me>
2024-03-16 14:00:39 -0500

    insert dnode

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,24 +9,15 @@
  *Return: adress of the new node
  */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *drifter = *head;
+	dlistint_t *drifter = *h;
 	dlistint_t *new_node;
 
 	if (idx == 0)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		add_dnodeint(h, n);
 	}
 
 	for (i = 0; drifter != NULL && i < idx - 1; i++)
