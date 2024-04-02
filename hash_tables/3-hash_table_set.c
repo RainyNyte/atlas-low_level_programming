6d66d925fd26133fd007c55c48fc5f9d187878b6
Rain Nyte <rain_nyte@proton.me>
2024-04-02 18:57:29 -0500

    fixed malloc handling

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,14 +12,18 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node;
 	hash_node_t *current = ht->array[index];
 
 	if (ht == NULL || key == NULL || *key == '\0')
+	{
 		return (0);
+	}
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
 		return (0);
-
+	}
 	new_node->key = strdup(key);
 
 	if (new_node->key == NULL)
