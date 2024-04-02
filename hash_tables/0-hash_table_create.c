8178fdc1eae33c6221b2137dfeb28a296ce6de9c
Rain Nyte <rain_nyte@proton.me>
2024-04-02 18:29:56 -0500

    Hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hash_tables.h"
-
-hash_table_t *hash_table_create(unsigned long int size) 
+/**
+ * hash_table_create - creates hash table
+ * @size: number of elements
+ * Return: hash
+ */
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-	
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-	if (table == NULL) 
-	{
-        	return NULL;
-	}
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
 
-	table->size = size;
-	table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
-	if (table->array == NULL)
-	{
-        	free(table);
-        	return NULL;
-	}
-	for (i = 0; i < size; ++i) 
-	{
-        	table->array[i] = NULL;
-	}
+	if (hash == NULL)
+		return (NULL);
 
-	return table;
+	hash->array = calloc(size, sizeof(hash_node_t *));
+	if (hash->array == NULL)
+	{
+		free(hash);
+		return (NULL);
+	}
+	hash->size = size;
+	return (hash);
 }
