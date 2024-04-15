33c92bb2bfe16f948468e1aca2d14e65d6e757a6
Rain Nyte <rain_nyte@proton.me>
2024-04-14 20:54:49 -0500

    betty compliance

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -17,17 +17,13 @@ int binary_search(int *array, size_t size, int value)
 	int right = size - 1;
 	int mid;
 
-	/* Check if the array is NULL */
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	/* Loop until left pointer is less than or equal to right pointer */
 	while (left <= right)
 	{
-		/*calculate the middle index*/
 		mid = left + (right - left) / 2;
-        	/* Print the array being searched */
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
@@ -38,23 +34,18 @@ int binary_search(int *array, size_t size, int value)
 			}
 		}
 		printf("\n");
-        	/* Check if the value is present at mid */
 		if (array[mid] == value)
 		{
 			return (mid);
 		}
-        	/* If value is greater, ignore left half */
 		else if (array[mid] < value)
 		{
 			left = mid + 1;
 		}
-        	/* If value is smaller, ignore right half */
 		else 
 		{
 			right = mid - 1;
 		}
 	}
-
-	/* If value is not present, return -1 */
 	return (-1);
 }
