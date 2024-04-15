adc0cc722d7a6b933b8c266b2af4394ea13a4760
Rain Nyte <rain_nyte@proton.me>
2024-04-14 20:20:12 -0500

    fixed declaration of variable

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,25 +2,22 @@
 
 int linear_search(int *array, size_t size, int value) 
 {
-    // Check if the array is NULL
+	size_t i;
+	
 	if (array == NULL) 
 	{
         	return (-1);
 	}
 
-    // Iterate through the array
-	for (size_t i = 0; i < size; ++i) 
+	for (i = 0; i < size; ++i) 
 	{
-        // Print the value being compared
        		printf("Comparing %d\n", array[i]);
         
-        // If the current element equals the value, return its index
         	if (array[i] == value) 
 		{
 			return (i);
         	}
 	}
 
-    // If the value is not found, return -1
 	return (-1);
 }
