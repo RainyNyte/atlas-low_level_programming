4c7b4f6652e149cbfc8c2706d14e5a55da4f3ee7
Rain Nyte <rain_nyte@proton.me>
2024-04-14 20:24:35 -0500

    fixed output formatting

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
 int linear_search(int *array, size_t size, int value) 
 {
 	size_t i;
-	
+
 	if (array == NULL) 
 	{
         	return (-1);
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; ++i) 
 	{
-       		printf("Comparing %d\n", array[i]);
+       		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
         
         	if (array[i] == value) 
 		{
