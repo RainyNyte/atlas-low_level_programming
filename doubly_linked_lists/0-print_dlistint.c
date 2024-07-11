6b5f9fcd5d0241d117d1a598c2eed3a1a0cdc3d6
Rain Nyte <rain_nyte@proton.me>
2024-07-11 13:12:13 -0500

    fixed error in typedef for the list struct

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,23 +9,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nd = 0;
-	const list_t *printer_in = h;
+	size_t n = 0;
+	const dlistint_t *printer_in = h;
 
 	while (printer_in != NULL)
 	{
-		if (printer_in->n == NULL)
-		{
-			printf("[0] (nil)\n");
-			printer_in = printer_in->next;
-			nd++;
-		}
-		else
-		{
-			printf("%d\n", printer_in->n);
-			printer_in = printer_in->next;
-			nd++;
-		}
+		printf("%d\n", printer_in->n);
+		printer_in = printer_in->next;
+		n++;
 	}
-	return (nd);
+	return (n);
 }
