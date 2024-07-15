2819ad4cb4345a805637f4fba68b0087a3192fe1
Rain Nyte <rain_nyte@proton.me>
2024-07-14 20:10:02 -0500

    function to add a node to a doubly linked list

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node to a doubly linked list
+ * @head: pointer to the head of the list
+ * @n: value to add to the list
+ *
+ * Return: adress of the new node, or NULL if failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+}
