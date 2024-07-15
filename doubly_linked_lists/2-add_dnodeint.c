7ce53159c01fd5a3b6f6a661192a26b83a993cf8
Rain Nyte <rain_nyte@proton.me>
2024-07-14 20:16:48 -0500

    Eddited function for adding a node so it can operate on an empty list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -19,9 +19,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	else
 	{
 		new_node->n = n;
-		new_node->next = *head;
 		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
 	}
+	if(*head == NULL)
+	{
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
+	}
+
+	return (new_node);
 }
